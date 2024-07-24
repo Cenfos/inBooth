f1e9061271c97fe87a8b0bc235ad3efb36726835
Cenfos <camilo.rguez@gmail.com>
2024-07-24 21:25:27 +0200

    Agregados botones funcionando de idiomas

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+    i18n: {
+        defaultLocale: "es",
+        locales: ["es", "en"],
+        routing: {
+            prefixDefaultLocale: false
+        }
+    },
+    fallback: {
+        'en': 'es'
+    }
+});
---
+++ src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.traduccion": "Translation",
+    "nav.interpretacion": "Interpretation",
+    "nav.contacto": "Contact",
+    "nav.about": "About",
+    "nav.instagram": "Instagram",
+  },
+  es: {
+    "nav.home": "Inicio",
+    "nav.traduccion": "Traducción",
+    "nav.interpretacion": "Interpretación",
+    "nav.contacto": "Contacto",
+    "nav.about": "Acerca de",
+    "nav.instagram": "Instagram",
+  },
+} as const;
